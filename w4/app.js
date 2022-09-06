eaacda1b446a893303d8230f7abc9d6e5c0b6e0e
Jacob Alvarez <jacob.alvarez.se@gmail.com>
2022-09-06 15:34:53 -0700

    My code week 4 learn together

--- w4/app.js
+++ w4/app.js
@@ -7,31 +7,32 @@ const displayOnPage = function (text) {
 
 const checkList = {
 
-    
+    roomClean: true, 
+    hwStatus: false, 
     checkRoom: function(x = 'undefined'){ //This function takes the mintues and converts it to hours
-        let roomClean
+        //let roomClean
         if(x === "clean"){
             displayOnPage('Your room is clean')
-             roomClean = true
+             this.roomClean = true
         }else {
             displayOnPage('Your room is dirty')
-             roomClean = false
+             this.roomClean = false
         
         }
 },
     checkAssignments: function(x){ 
-        let hwStatus
+        //let hwStatus
         if(x === 'complete'){
             displayOnPage('Your homework is done')
-             hwStatus = true //global varible 
+             this.hwStatus = true //global varible 
         }else {
             displayOnPage("Your homework isn't complete")
-             hwStatus = false
+             this.hwStatus = false
         
         }
     },
-    hasFreeTime: function(roomClean, hwStatus){
-        if(roomClean & hwStatus){
+    hasFreeTime: function(){
+        if(this.roomClean & this.hwStatus){
             displayOnPage('You are free for the rest of the day')
         } else {
             displayOnPage('You must finish your HW and or room')
@@ -45,7 +46,7 @@ checkList.checkRoom('clean')
 displayOnPage('<b> Is your homework done?</b>') 
 checkList.checkAssignments('complete')
 displayOnPage('<b> Do you have free time?</b>') 
-checkList.hasFreeTime(roomClean, hwStatus)
+checkList.hasFreeTime()
 
 
 
