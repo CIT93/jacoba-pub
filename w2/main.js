60fda10a2a5a21d00bbbdea8b4bee208f2b09b08
JacobAlvarezSe <jacob.alvarez.se@gmail.com>
2024-01-21 21:11:06 -0800

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,13 @@
-houseMembers = 
+const houseMembers = 10 // 2 in household
+const houseSize = 7 //medium
+const foodChoice = 12 // mostly prepackaged
+const waterUseage = 2 // 4-9 times
+const housePurchases = 2
+const garbageCans = 30
+const wasteRecycle = 8
+const annualMileage = 12
+
+const totatCarbonFootprint = houseMembers + houseSize + foodChoice +wasteRecycle + waterUseage + housePurchases +garbageCans + annualMileage
+
+const myHeading = document.querySelector('h1')
+myHeading.textContent = totatCarbonFootprint; 
