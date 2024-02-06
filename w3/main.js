ad1a92408bd42672a4242b50a7298ef05829be09
JacobAlvarezSe <jacob.alvarez.se@gmail.com>
2024-02-05 18:33:50 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -1,24 +1,15 @@
 let carbonfrootprintPoints = 0;
 
-function determineHousepointPts (numberInHousehold) {
-        if(numberInHousehold == 1) {
-        carbonfrootprintPoints = carbonfrootprintPoints +14;
-    }   else if(numberInHousehold ==2){
-        carbonfrootprintPoints = carbonfrootprintPoints +12
-    }   else if(numberInHousehold ==3){
-        carbonfrootprintPoints = carbonfrootprintPoints +10
-    }   else if(numberInHousehold ==4){
-        carbonfrootprintPoints = carbonfrootprintPoints +8
-    }   else if(numberInHousehold ==5){
-        carbonfrootprintPoints = carbonfrootprintPoints +6
-    }   else if(numberInHousehold ==6){
-        carbonfrootprintPoints = carbonfrootprintPoints +4
-    }   else if(numberInHousehold ==5){
-        carbonfrootprintPoints = carbonfrootprintPoints +2
-    }   
-    console.log(`based on the number in the Household of ${numberInHousehold} the points would be ${carbonfrootprintPoints}`)
-}
-determineHousepointPts(3);
-determineHousepointPts(4);
-
-
+function determineHouseSize(houseSize){
+    if(houseSize == 'Small'){
+        carbonfrootprintPoints = carbonfrootprintPoints + 7;
+    } else if (houseSize == "Medium"){
+        carbonfrootprintPoints = carbonfrootprintPoints + 4;
+     } else if (houseSize == 'Large'){
+        carbonfrootprintPoints = carbonfrootprintPoints + 10;
+     }else { //apartment
+        carbonfrootprintPoints = carbonfrootprintPoints + 2;
+     }
+     console.log(`Base on your home size your carbonfootprint points would add ${carbonfrootprintPoints}`)
+    }
+determineHouseSize('Large')
